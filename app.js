63b00904b0c9107cd39cc8f038d887c50fd589ba
ludmiraminegocio-byte <ludmirami.negocio@gmail.com>
2026-08-27 03:57:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const URL_API_GOOGLE = 'https://script.google.com/macros/s/AKfycbylouzWyPDDuWpKMQLKQzi6erN_KpfHvNdb-osX21DDZwfunF091rnSCRIHpFfED0g/exec';
+const URL_API_GOOGLE = 'https://script.google.com/macros/s/AKfycbwTeLUcL29YGo-k_2GiHGY7k-nIZGtbXyDRHMQpeYDiQpIpEtXLFzktqPyd01edjrXX/exec';
 
 // Lista de productos del catálogo
 const productos = [
